cdbe6a1d5aee7907d85bf98a6db2b932b692cb3d
Samuel <sheldak426@gmail.com>
2020-04-01 23:18:38 +0200

    added a few functions to pollution module

--- src/lab_2/pollution.erl
+++ src/lab_2/pollution.erl
@@ -10,17 +10,9 @@
 -author("sheldak").
 
 %% API
--export([createMonitor/0, addStation/3, addValue/5, listLen/1, isStation/2]).
+-export([createMonitor/0, addStation/3, addValue/5, removeValue/4, getOneValue/4, getStationMean/3, getDailyMean/3]).
 
 createMonitor() -> #{}.
-
-
-addStation(Name, {X, Y}, Monitor) ->
-  Key = {X, Y},
-  case maps:find({X, Y}, Monitor) of
-    {ok, _} -> {error, duplicated_station};
-    _ -> Monitor#{Key => #{name => Name}}
-  end.
 
 
 listLen([]) -> 0;
@@ -33,7 +25,14 @@ isStation(Name, Monitor) ->
 
   case listLen(ListWithStation) of
     0 -> false;
-    _  -> true
+    _ -> true
+  end.
+
+
+addStation(Name, {X, Y}, Monitor) ->
+  case maps:is_key({X, Y}, Monitor) or isStation(Name, Monitor) of
+    true -> error(duplicated_station);
+    _    -> Monitor#{{X, Y} => #{name => Name}}
   end.
 
 
@@ -43,16 +42,84 @@ findKey(Name, Monitor) ->
 
   K.
 
-% TODO check if value has already been added before to avoid duplications
+
 addValue({X, Y}, Date, Type, Value, Monitor) ->
-  Key = {X, Y},
-  #{Key := PrevVal} = Monitor,
   case maps:is_key({X, Y}, Monitor) of
-    true  -> Monitor#{{X, Y} := PrevVal#{{Date, Type} => Value}};
-    false -> {error, no_station}
+    true -> StationValues = maps:get({X, Y}, Monitor),
+
+            case maps:is_key({Date, Type}, StationValues) of
+              true -> error(duplicated_value);
+              _    -> Monitor#{{X, Y} => maps:put({Date, Type}, Value, StationValues)}
+            end;
+    _    -> error(no_station)
   end;
+
 addValue(Name, Date, Type, Value, Monitor) ->
   case isStation(Name, Monitor) of
-    false -> {error, no_station};
-    _     -> addValue(findKey(Name, Monitor), Date, Type, Value, Monitor)
+    true -> addValue(findKey(Name, Monitor), Date, Type, Value, Monitor);
+    _    -> error(no_station)
   end.
+
+
+removeValue({X, Y}, Date, Type, Monitor) ->
+  case maps:is_key({X, Y}, Monitor) of
+    true -> StationValues = maps:get({X, Y}, Monitor),
+            Monitor#{{X, Y} => maps:remove({Date, Type}, StationValues)};
+    _    -> error(no_station)
+  end;
+
+removeValue(Name, Date, Type, Monitor) ->
+  case isStation(Name, Monitor) of
+    true -> removeValue(findKey(Name, Monitor), Date, Type, Monitor);
+    _    -> error(no_station)
+  end.
+
+
+getOneValue({X, Y}, Date, Type, Monitor) ->
+  case maps:is_key({X, Y}, Monitor) of
+    true -> StationValues = maps:get({X, Y}, Monitor),
+
+            case maps:is_key({Date, Type}, StationValues) of
+              true -> maps:get({Date, Type}, StationValues);
+              _    -> error(no_value)
+            end;
+    _    -> error(no_station)
+  end;
+
+getOneValue(Name, Date, Type, Monitor) ->
+  case isStation(Name, Monitor) of
+    true -> getOneValue(findKey(Name, Monitor), Date, Type, Monitor);
+    _    -> error(no_station)
+  end.
+
+
+getStationMean({X, Y}, Type, Monitor) ->
+  case maps:is_key({X, Y}, Monitor) of
+    true -> StationValues = maps:get({X, Y}, Monitor),
+            StationMeasurements = maps:filter(fun({_D, T}, _V) -> T == Type; (_, _) -> false end, StationValues),
+            MeasurementsNumber = max(maps:size(StationMeasurements), 1),
+            MeasurementsSum = maps:fold(fun(_K, V, Acc) -> V + Acc end, 0, StationMeasurements),
+            MeasurementsSum / MeasurementsNumber;
+    _    -> error(no_station)
+  end;
+
+getStationMean(Name, Type, Monitor) ->
+  case isStation(Name, Monitor) of
+    true -> getStationMean(findKey(Name, Monitor), Type, Monitor);
+    _    -> error(no_station)
+  end.
+
+
+getDailyMean(Day, Type, Monitor) ->
+  DayTypeFilterPred = fun ({{D, _H}, T}, _V) -> X = T == Type,
+                                                Y = D == Day,
+                                                X and Y;
+                            (_, _)           -> false
+                      end,
+  FilteredMonitor = maps:map(fun(_K, V) -> maps:filter(DayTypeFilterPred, V) end, Monitor),
+
+  FoldStationFunc = fun(_K, V, Acc) -> Acc + V end,
+  DailySum = maps:fold(fun(_K, V, Acc) -> Acc + maps:fold(FoldStationFunc, 0, V) end, 0, FilteredMonitor),
+  DailySize = max(maps:fold(fun(_K, V, Acc) -> Acc + maps:size(V) end, 0, FilteredMonitor), 1),
+
+  DailySum / DailySize.
